7c47e9abc2740d1ded735f38c4e1f3450bab6f43
Giovane <giovanedaniell@gmail.com>
2022-09-17 17:17:03 -0300

    style: create styled components for Main component

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+`
+
+export const Logo = styled.img``
+
+export const Title = styled.h1``
+
+export const Description = styled.h2``
+
+export const Illustration = styled.img``
