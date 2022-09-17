a00d3b571c499393a9c277b41c8c178ea1b75188
Giovane <giovanedaniell@gmail.com>
2022-09-17 19:23:32 -0300

    feat: add manifest.json on document <head />

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>Next + TypeScript - Boilerplate</title>
         <link rel="shortcut-icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A boilerplate with everything configured to work with TypeScript, React, NextJS, Jest, Testing Library, Storybook and StyledComponents"
