782e1b2a9f66091bb97f6697e7e4b39d19707f02
Giovane <giovanedaniell@gmail.com>
2022-09-17 17:23:48 -0300

    style: finish styles for Main component

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -5,12 +5,29 @@ export const Wrapper = styled.main`
   color: #fff;
   width: 100%;
   height: 100%;
+  padding: 3rem;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `
 
-export const Logo = styled.img``
+export const Logo = styled.img`
+  width: 25rem;
+  margin-bottom: 2rem;
+`
 
-export const Title = styled.h1``
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
 
-export const Description = styled.h2``
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
 
-export const Illustration = styled.img``
+export const Illustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
+`
