99b4ab73234e851f59f6508cab22cd21bb105f38
Giovane <giovanedaniell@gmail.com>
2022-09-17 19:30:12 -0300

    fix: configs for jest
    jest not collecting coverage from ts files and collecting coverage from stories

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const config = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['/src/**/*.ts(x)'],
+  collectCoverageFrom: ['/src/**/*.ts(x)?', 'src/**/!stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
 
