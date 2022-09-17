8f2af1022a52e2f1c746a7fd37e2b4e0a616f8f5
Giovane <giovanedaniell@gmail.com>
2022-09-17 19:01:17 -0300

    refactor: add props to Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,19 @@
 import { FC } from 'react'
 import * as S from './styles'
 
-const Main: FC = () => (
+interface IMain {
+  title?: string
+  description?: string
+}
+
+const Main: FC<IMain> = ({
+  title = 'Boilerplate React + NextJS',
+  description = 'TypeScript, React, NextJS, Jest, Testing Library, Storybook and Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo src="img/logo.svg" alt="Athom image with a Boilerplate text" />
-    <S.Title>Boilerplate React + NextJS</S.Title>
-    <S.Description>
-      TypeScript, React, NextJS, Jest, Testing Library, Storybook and Styled
-      Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description> {description} </S.Description>
     <S.Illustration src="img/hero-illustration.svg" alt="Developer coding" />
   </S.Wrapper>
 )
