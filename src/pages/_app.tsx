9381d039ac93efa15be03ddddd8c744a2e6350c5
Giovane <giovanedaniell@gmail.com>
2022-09-17 17:16:08 -0300

    feat: create App component

---
+++ src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import GlobalStyles from 'styles/global'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next + TypeScript - Boilerplate</title>
+        <link rel="shortcut-icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta
+          name="description"
+          content="A boilerplate with everything configured to work with TypeScript, React, NextJS, Jest, Testing Library, Storybook and StyledComponents"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
