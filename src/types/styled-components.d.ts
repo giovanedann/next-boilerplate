973b55910b12f7af34226852a73f5fb9e2e59009
Giovane <giovanedaniell@gmail.com>
2022-09-18 15:34:38 -0300

    style: add comments to styled-components declaration file

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -1,4 +1,10 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
-declare module 'styled-components' {
-  export interface DefaultTheme {}
+
+// place your theme interface here (or in another folder)
+export interface Theme {}
+
+declare global {
+  module 'styled-components' {
+    interface DefaultTheme extends Theme {} // extends your theme interface
+  }
 }
