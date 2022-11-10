9728b7a3b986104c0ee04da8cc6ab736e6a53cb8
Giovane <giovanedaniell@gmail.com>
2022-11-10 19:41:38 -0300

    feat(stories): create stories for Main component

--- src/components/Main/stories.stories.tsx
+++ src/components/Main/stories.stories.tsx
@@ -7,12 +7,14 @@ export default {
   title: 'Main',
   component: Main,
   argTypes: {
-    title: { control: { type: 'string' } },
-    description: { control: 'string' }
+    // place your args types here (inputs of type color, radio, etc)
   }
 } as ComponentMeta<typeof Main>
 
 const Template: ComponentStory<typeof Main> = (args) => <Main {...args} />
 
 export const Basic = Template.bind({})
-Basic.args = {}
+Basic.args = {
+  description: 'This is an example of description',
+  title: 'This is an example of title'
+}
