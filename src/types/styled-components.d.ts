430581e9cb367c1d3e409df0c473578ba919c66c
Giovane <giovanedaniell@gmail.com>
2022-12-21 09:41:00 -0300

    refactor: change declarative types of styled-components

--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -3,8 +3,6 @@
 // place your theme interface here (or in another folder)
 export interface Theme {}
 
-declare global {
-  module 'styled-components' {
-    interface DefaultTheme extends Theme {} // extends your theme interface
-  }
+declare module 'styled-components' {
+  interface DefaultTheme extends Theme {} // extends your theme interface
 }
